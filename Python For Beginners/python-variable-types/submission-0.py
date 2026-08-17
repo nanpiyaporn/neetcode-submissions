2b2a66a0973afa858b6cb61f194fc0826e6a1600
Piyaporn Puangprasert (Nan) <dngooddeals@gmail.com>
2026-08-17 19:41:49 -0400

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,11 @@
+integer_type = 25
+float_type = 23.1
+bool_type = True
+string_type = "A"
+list_type = [2,3,5]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
